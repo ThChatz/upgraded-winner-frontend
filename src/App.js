34055ef95eca8dd7e9fe7ca57daf4a3e5d8cc041
vasilisp100 <vasilis.p.100@gmail.com>
2021-08-03 02:49:52 +0300

    implemented a navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import NavBar from './components/navbar';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Toast from 'react-bootstrap/Toast';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col'
+
 import './App.css';
 
-function App() {
+const ExampleToast = ({ children }) => {
+  const [show, toggleShow] = useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <>
+        {!show && <Button onClick={() => toggleShow(true)}>Show Toast</Button>}
+        <Toast show={show} onClose={() => toggleShow(false)}>
+          <Toast.Header>
+            <strong className="mr-auto">React-Bootstrap</strong>
+          </Toast.Header>
+          <Toast.Body>{children}</Toast.Body>
+        </Toast>
+      </>
     </div>
   );
-}
+};
+
+const App = () => (
+  <Container className="p-3">
+    <NavBar/>
+    
+    <Container>
+      <Row className="justify-content-md-center">
+        {/* <Col xs lg="2">
+          1 of 3
+        </Col>
+        <Col md="auto">Variable width content</Col>
+        <Col xs lg="2">
+          3 of 3
+        </Col> */}
+      </Row>
+    </Container>
+  </Container>
+);
 
 export default App;
---
+++ src/components/navbar.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import { Button } from 'bootstrap';
+import SearchField from "react-search-field";
+import { BsFillHouseDoorFill, BsPeopleFill, BsBriefcaseFill, BsFillCursorFill, BsFillBellFill } from "react-icons/bs";
+
+class NavBar extends Component{
+    render() {
+        return (
+            <>
+            <Navbar className="navbar navbar-expand-lg navbar-light fixed-top shadow-sm">
+                <Container className="container px-5">
+                <Navbar.Brand className="navbar-brand fw-bold" href="#home">Linked out</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                    <SearchField
+                        placeholder="Search..."
+                        // onChange={onChange}
+                        searchText="Search..."
+                        classNames="test-class"
+                    />
+                    </Nav>
+                    <Nav>
+                        <Nav.Link className="nav-item" href="#Home"><BsFillHouseDoorFill size="30" title="Home" /><br/></Nav.Link>
+                        <Nav.Link className="nav-item" href="#Network"><BsPeopleFill size="30" title="Home" /><br/></Nav.Link>
+                        <Nav.Link className="nav-item" href="#Jobs"><BsBriefcaseFill size="30" title="Home" /><br/></Nav.Link>
+                        <Nav.Link className="nav-item" href="#Messaging"><BsFillCursorFill size="30" title="Home" /><br/></Nav.Link>
+                        <Nav.Link className="nav-item" href="#Notifications"><BsFillBellFill size="30" title="Home" /><br/></Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+                </Container>
+            </Navbar>     
+            <br/>
+            <br/>  
+            </>
+        );
+    }
+}
+
+export default NavBar;
+
