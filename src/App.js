33873663e124f753f130ab18f82b1ecdb54a7293
vasilisp100 <vasilis.p.100@gmail.com>
2021-08-07 00:25:03 +0300

    added components:
    - signup/signin
    - create post

--- src/App.js
+++ src/App.js
@@ -1,48 +1,16 @@
-import React, { useState } from 'react';
-import NavBar from './components/navbar';
-import Jumbotron from 'react-bootstrap/Jumbotron';
-import Toast from 'react-bootstrap/Toast';
-import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col'
-
+import React from 'react';
 import './App.css';
 
-const ExampleToast = ({ children }) => {
-  const [show, toggleShow] = useState(true);
-
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
+import CreatePost from './components/CreatePost/CreatePost';
+function App() {
   return (
-    <div>
-      <>
-        {!show && <Button onClick={() => toggleShow(true)}>Show Toast</Button>}
-        <Toast show={show} onClose={() => toggleShow(false)}>
-          <Toast.Header>
-            <strong className="mr-auto">React-Bootstrap</strong>
-          </Toast.Header>
-          <Toast.Body>{children}</Toast.Body>
-        </Toast>
-      </>
+    <div className="App">
+      
+     <CreatePost/>
     </div>
   );
-};
-
-const App = () => (
-  <Container className="p-3">
-    <NavBar/>
-    
-    <Container>
-      <Row className="justify-content-md-center">
-        {/* <Col xs lg="2">
-          1 of 3
-        </Col>
-        <Col md="auto">Variable width content</Col>
-        <Col xs lg="2">
-          3 of 3
-        </Col> */}
-      </Row>
-    </Container>
-  </Container>
-);
+}
 
 export default App;
---
+++ src/components/CreatePost/CreatePost.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import './CreatePost.css'
+// icons
+import { Avatar } from '@material-ui/core'
+import { CameraAlt, VideoCall, CalendarToday, Assignment } from '@material-ui/icons'
+const CreatePost = () => {
+  
+    return (
+        <div className="messageSender">
+            <div className="messageSenderTop">
+                <Avatar>H</Avatar>
+                <form>
+                    <input 
+                        value=""
+                        className="messageSenderInput" 
+                        placeholder={`What's on your mind?`} 
+                    />
+                   
+                    <button  type="submit"></button>
+                </form>
+            </div>
+
+            <div className="messageSenderBottom">
+                <div className="messageSenderOption">
+                    <VideoCall />
+                    <h3>Video</h3>
+                </div>
+
+                <div className="messageSenderOption">
+                    <CameraAlt />
+                    <h3>Photo</h3>
+                </div>
+
+                <div className="messageSenderOption">
+                    <CalendarToday />
+                    <h3>Event</h3>
+                </div>
+
+                <div className="messageSenderOption">
+                    <Assignment />
+                    <h3>Write Article</h3>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CreatePost;
