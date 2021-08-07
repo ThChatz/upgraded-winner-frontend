a09970f46cfe729d6876bde1649303e719a8f9de
vasilisp100 <vasilis.p.100@gmail.com>
2021-08-07 18:06:42 +0300

    fixed navbar

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import './App.css';
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
 import CreatePost from './components/CreatePost/CreatePost';
+import NavBar from './components/navbar';
 function App() {
   return (
-    <div className="App">
-      
-     <CreatePost/>
+    <div>
+      <NavBar/>
+      <div className="App">
+      <CreatePost/>
+      </div>
     </div>
   );
 }
