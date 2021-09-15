528e86b2a93bf504357ddffaa011647c63e18c41
vasilisp100 <vasilis.p.100@gmail.com>
2021-09-16 00:51:48 +0300

    Expanded sign up form

--- src/App.js
+++ src/App.js
@@ -13,36 +13,36 @@ import WelcomePageView from './views/WelcomePageView';
 import get from 'axios';
 
 const RequireAuth = (props) => {
-    const [pageContents, setPageContents] = useState(<></>);
+	const [pageContents, setPageContents] = useState(<></>);
 
-    useEffect(() =>
-	get("/session")
-	    .then((resp) => resp.status === 200 && setPageContents(props.children))
-	    .catch((resp) => setPageContents(<WelcomePageView />)),
-	[props.children])
-    return <>{pageContents}</>;
+	useEffect(() =>
+		get("/session")
+			.then((resp) => resp.status === 200 && setPageContents(props.children))
+			.catch((resp) => setPageContents(<WelcomePageView />)),
+		[props.children])
+	return <>{pageContents}</>;
 }
-      
+
 
 const App = (props) =>
 
-		<HashRouter>
-			<Switch>
-				<Route path="/" component={WelcomePageView} exact />
-				<Route path="/signup" component={SignUpView} exact />
-				<Route path="/login" component={LoginView} exact />
-			    	<RequireAuth>
-				    <Route path="/home" component={FeedView} exact />
-				    <Route path="/u/:user_id/" exact>
+	<HashRouter>
+		<Switch>
+			<Route path="/" component={FeedView} exact />
+			<Route path="/signup" component={SignUpView} exact />
+			<Route path="/login" component={LoginView} exact />
+			<RequireAuth>
+				<Route path="/home" component={FeedView} exact />
+				<Route path="/u/:user_id/" exact>
 					<ProfileView />
-				    </Route>
-				    <Route path="/network" component={NetworkView} exact />
-				    <Route path="/jobs" component={JobView} exact />
-				    <Route path="/messages/:thread_id" component={MessagesView} exact />
-				</RequireAuth>
-				</Switch>
-		</HashRouter>
+				</Route>
+				<Route path="/network" component={NetworkView} exact />
+				<Route path="/jobs" component={JobView} exact />
+				<Route path="/messages/:thread_id" component={MessagesView} exact />
+			</RequireAuth>
+		</Switch>
+	</HashRouter>
 
 
 export default App;
- 
+
--- src/views/SignUpView.js
+++ src/views/SignUpView.js
@@ -1,54 +1,109 @@
 import React, { Component } from "react";
 import { submitHandler } from "../submitForm.js";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Form from "react-bootstrap/Form";
 
 
 export default class SignUp extends Component {
     render() {
-	const onSubmit = submitHandler("post", "/user");
+        const onSubmit = submitHandler("post", "/user");
 
         return (
             <div className="auth-wrapper">
                 <div className="auth-inner">
-                    <form onSubmit={onSubmit}>
+                    <Form onSubmit={onSubmit}>
                         <h3>Sign Up</h3>
 
-                        <div className="form-group">
-                            <label>First name</label>
-                            <input type="text"
-				   name="first-name"
-				   className="form-control"
-				   placeholder="First name" />
-                        </div>
+                        <Form.Group>
 
-                        <div className="form-group">
-                            <label>Last name</label>
-                            <input type="text"
-				   name="last-name"
-				   className="form-control"
-				   placeholder="Last name" />
-                        </div>
+                            <Form.Label>Profile picture</Form.Label>
+                            <Form.Control type="file" name="picture" />
 
-                        <div className="form-group">
-                            <label>Email address</label>
-                            <input type="email"
-				   name="email"
-				   className="form-control"
-				   placeholder="Enter email" />
-                        </div>
+                            <Form.Label>First name</Form.Label>
+                            <Form.Control type="text"
+                                name="first-name"
+                                placeholder="First name" />
 
-                        <div className="form-group">
-                            <label>Password</label>
-                            <input type="password"
-				   name="password"
-				   className="form-control"
-				   placeholder="Enter password" />
-                        </div>
-                        <br/>
+                            <Form.Label>Last name</Form.Label>
+                            <Form.Control type="text"
+                                name="last-name"
+                                placeholder="Last name" />
+
+                            <Form.Label>Email address</Form.Label>
+                            <Row>
+                                <Col>
+                                    <Form.Control type="email"
+                                        name="email"
+                                        placeholder="Enter email" />
+                                </Col>
+                                <Col xs={3} value={false}>
+                                    <Form.Control as="select" name="email-private">
+                                        <option value={false}>private</option>
+                                        <option value={true}>public</option>
+                                    </Form.Control>
+                                </Col>
+                            </Row>
+
+                            <Form.Label>Phone Number</Form.Label>
+                            <Row>
+                                <Col>
+                                    <Form.Control type="text"
+                                        name="phone"
+                                        placeholder="Enter phone number" />
+                                </Col>
+                                <Col xs={3} value={false}>
+                                    <Form.Control as="select" name="phone-private">
+                                        <option value={false}>private</option>
+                                        <option value={true}>public</option>
+                                    </Form.Control>
+                                </Col>
+                            </Row>
+
+                            <Form.Label>Current Job</Form.Label>
+                            <Row>
+                                <Col>
+                                    <Form.Control type="text"
+                                        name="job"
+                                        placeholder="Enter your current job position" />
+                                </Col>
+                                <Col xs={3} value={false}>
+                                    <Form.Control as="select" name="job-private">
+                                        <option value={false}>private</option>
+                                        <option value={true}>public</option>
+                                    </Form.Control>
+                                </Col>
+                            </Row>
+
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password"
+                                name="password"
+                                placeholder="Enter your password" />
+                            <Form.Label>Confirm password</Form.Label>
+                            <Form.Control type="password"
+                                placeholder="Retype your password" />
+
+                        </Form.Group>
+
+                        <Form.Control as="select" name="bio-private">
+                            <option value={false}>private</option>
+                            <option value={true}>public</option>
+                        </Form.Control>
+
+                        <Form.Control as="select" name="network-private">
+                            <option value={false}>private</option>
+                            <option value={true}>public</option>
+                        </Form.Control>
+
+                        <Form.Control type="text" name="bio" />
+
+                        <br />
                         <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
                         <p className="forgot-password text-right">
                             Already registered <a href="#/login">sign in?</a>
                         </p>
-                    </form>
+                    </Form>
                 </div>
             </div>
         );
