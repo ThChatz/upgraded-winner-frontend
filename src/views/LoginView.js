6d7698224cadc29384dac7fa32aad5bb9cbbe725
vasilisp100 <vasilis.p.100@gmail.com>
2021-09-19 19:22:59 +0300

    Changed login View to react bootstrap

--- src/views/LoginView.js
+++ src/views/LoginView.js
@@ -1,35 +1,38 @@
 import React, { Component } from "react";
 import '../App.css';
 import { submitHandler } from '../submitForm'
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Form from "react-bootstrap/Form";
 
 export default class Login extends Component {
     render() {
 
-	const onSubmit = submitHandler("post", "/session");
+        const onSubmit = submitHandler("post", "/session");
         return (
-            <div className = "App">    
-                <div className="auth-wrapper">
-                    <div className="auth-inner">
-                        <form onSubmit={onSubmit}>
-                            <h3>Sign In</h3>
+            <div className="auth-wrapper">
+                <div className="auth-inner">
+                    <Form onSubmit={onSubmit}>
+                        <h3>Sign In</h3>
+                        <Form.Group>
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email"
+                                name="email"
+                                placeholder="Enter Email" />
+                            
 
-                            <div className="form-group">
-                                <label>Email address</label>
-                                <input type="email" name="email"  className="form-control" placeholder="Enter email" />
-                            </div>
-
-                            <div className="form-group">
-                                <label>Password</label>
-                                <input type="password" name="password" className="form-control" placeholder="Enter password" />
-                            </div>
-
-                            <br/>
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password"
+                                name="password"
+                                placeholder="Enter your password" />
+                            <br />
                             <button type="submit" className="btn btn-primary btn-block">Submit</button>
                             <p >
                                 Forgot <a href="#">password?</a>
                             </p>
-                        </form>
-                    </div>
+                        </Form.Group>
+                    </Form>
                 </div>
             </div>
         );
