e2cb4a0d4e6c6b9c412533bf93c5553eaea64652
vasilisp100 <vasilis.p.100@gmail.com>
2021-09-28 01:35:48 +0300

    Created an admin navbar

---
+++ src/components/AdminNavBar.jsx
@@ -0,0 +1,23 @@
+import Navbar from 'react-bootstrap/Navbar';
+import { Container } from 'react-bootstrap';
+import { AiOutlineArrowUp } from "react-icons/ai"
+import { Nav } from 'react-bootstrap';
+function AdminNavBar() {
+    return (
+        <>
+            <Navbar className="navbar-light fixed-top shadow-sm d-flex justify-content-between py-0">
+                <Navbar.Brand className="navbar-brand fw-bold" href="#Home">
+                    <AiOutlineArrowUp style={{ "float": "left" }} size="60" />
+                    <text>Upgraded<br />Winner</text>
+                </Navbar.Brand>
+                <Nav></Nav>
+                <Nav>
+                    <Nav.Link href="#">Sign Out</Nav.Link>
+                </Nav>
+            </Navbar>
+            <br />
+            <br />
+        </>
+    );
+}
+export default AdminNavBar;
