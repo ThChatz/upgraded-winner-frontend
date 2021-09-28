d6daf35d6b21b7e23b81f443bd055ab95459dd92
vasilisp100 <vasilis.p.100@gmail.com>
2021-09-28 22:58:44 +0300

    CreateAdmin: added bootstrap to logo

--- src/views/CreateAdmin.jsx
+++ src/views/CreateAdmin.jsx
@@ -16,42 +16,40 @@ export default class CreateAdmin extends Component {
         return (
             <div className="auth-wrapper">
                 <div className="auth-inner">
-                    <Container style={{
-				   "height": "48px",
-				   "width": "155px"
-			       }}>
+                    <Container className="d-flex justify-content-center pb-sm-3">
+
                         <AiOutlineArrowUp style={{ "float": "left" }} size="50" />
                         <b>Upgraded<br />Winner</b>
 
                     </Container>
-		    <br />
-		    <h3>Admin Account Information</h3>
-			<SignUp target="/install">
-			    <Tab title="Personal" eventKey="Personal">
-				<SignUp.Personal />
-			    </Tab>
-			    <Tab title="Bio" eventKey="Bio">
-				<SignUp.Bio />
-			    </Tab>
-			    <Tab title="Privacy" eventKey="Privacy">
-				<SignUp.Privacy />
-			    </Tab>
-			    <Tab title="Password" eventKey="Password">
-				<SignUp.Password />
-			    </Tab>
-			    <Tab title="Picture" eventKey="ProfilePic">
-				<SignUp.ProfilePic />
-			    </Tab>
+                    <br />
+                    <h3>Admin Account Information</h3>
+                    <SignUp target="/install">
+                        <Tab title="Personal" eventKey="Personal">
+                            <SignUp.Personal />
+                        </Tab>
+                        <Tab title="Bio" eventKey="Bio">
+                            <SignUp.Bio />
+                        </Tab>
+                        <Tab title="Privacy" eventKey="Privacy">
+                            <SignUp.Privacy />
+                        </Tab>
+                        <Tab title="Password" eventKey="Password">
+                            <SignUp.Password />
+                        </Tab>
+                        <Tab title="Picture" eventKey="ProfilePic">
+                            <SignUp.ProfilePic />
+                        </Tab>
 
-			</SignUp>
+                    </SignUp>
 
-                        <br />
-			<div className="d-flex justify-content-around">
-                            <Button type="submit"
-				    variant="primary">
-				Finish
-			    </Button>
-			</div>
+                    <br />
+                    <div className="d-flex justify-content-around">
+                        <Button type="submit"
+                            variant="primary">
+                            Finish
+                        </Button>
+                    </div>
                 </div>
             </div>
         );
