7514460c9936df1aecffa9a4849aa377a7216a73
vasilisp100 <vasilis.p.100@gmail.com>
2021-10-01 02:35:05 +0300

    added friend requests listing

--- src/components/NetworkSuggestions/NetworkSuggestions.jsx
+++ src/components/NetworkSuggestions/NetworkSuggestions.jsx
@@ -1,23 +1,42 @@
 import UserInfo from "../UserInfo/UserInfo.js";
 import FetchScroll from "../FetchScroll/FetchScroll.jsx";
-import { ListGroup } from "react-bootstrap";
+import { Container, ListGroup } from "react-bootstrap";
 import { useState } from 'react';
 
 import get from 'axios';
 
 function NetworkSuggestions(props) {
 
-	const [list, setList] = useState([]);
+	const [reqList, setReqList] = useState([]);
 
-	get(`${process.env.REACT_APP_API_ROOT}${props.src}`, 
-	{withCredentials: true})
-	.then(resp => setList(resp.data))
+	get(`${process.env.REACT_APP_API_ROOT}/connections/requests`,
+		{ withCredentials: true })
+		.then(resp => setReqList(resp.data))
+
+
+	const [friendlist, setFriendList] = useState([]);
+
+	get(`${process.env.REACT_APP_API_ROOT}/connections`,
+		{ withCredentials: true })
+		.then(resp => setFriendList(resp.data))
 
 	return (
-		list.map((item) =>
-		<ListGroup.Item key={item.friend} >
-			<UserInfo.byId id={item.friend}/>
-		</ListGroup.Item>)
+		<>
+			<Container className="shadow p-3 mb-5 bg-white rounded">
+				<h3>Friend Requests</h3>
+				{reqList.map((item) =>
+					<ListGroup.Item key={item.friend} >
+						<UserInfo.byId id={item.friend} />
+					</ListGroup.Item>)}
+			</Container>
+			<Container className="shadow p-3 mb-5 bg-white rounded">
+				<h3>Friends</h3>
+				{friendlist.map((item) =>
+					<ListGroup.Item key={item.friend} >
+						<UserInfo.byId id={item.friend} />
+					</ListGroup.Item>)}
+			</Container>
+		</>
 	)
 }
 
